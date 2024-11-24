ae996e44115b299f0f83dcb2cf7837948bfac6ff
Max Sveshnikov <msveshnikov@gmail.com>
2024-11-24 21:47:06 +0100

    WIP

---
+++ server/unsplash.js
@@ -0,0 +1,16 @@
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const getUnsplashImages = async (prompt) => {
+    const response = await fetch(
+        `https://api.unsplash.com/search/photos?query=${prompt}&client_id=${process.env.UNSPLASH_API_KEY}&w=800`
+    );
+    if (!response.ok) {
+        throw new Error('Unsplash API error:' + response.status);
+    }
+    const data = await response.json();
+    return data.results.map((r) => r.urls.regular);
+};
+
+console.log(await getUnsplashImages('christmas'));
